987f206f64a75af3e20805b3cd7ff7ffc021d11b
dhwanibhut <controlplastic8@gmail.com>
2025-03-24 15:26:26 +0530

    Update databasew.ts

--- databasew.ts
+++ databasew.ts
@@ -6,6 +6,7 @@ async function initializeDatabase() {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    
   });
 
   // Your database initialization logic here
